482709da63a3dde85dda252369590904dfed7ba1
MarxJiao <marxjiao@gmail.com>
2017-07-25 19:44:40 +0800

    添加静态文件

--- source/sw.js
+++ source/sw.js
@@ -2,14 +2,17 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('marx').then(function (cache) {
             return cache.addAll([
-                '/'
+                '/',
+                '/css/fonts/fontawesome-webfont.woff?v=',
+                '/css/style.css',
+                '/js/script.js',
+                '/css/images/banner.jpg'
             ]);
         })
     );
 });
 
 self.addEventListener('fetch', function (event) {
-    console.log(event.request.url);
     event.respondWith(
         caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
